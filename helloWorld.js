47c126d8d379e78a7b69d83c4cbb7b51e834590e
shevourn peart <shevourn.peart@gmail.com>
2023-09-12 18:30:18 -0600

    add something new

---
+++ helloWorld.js
@@ -0,0 +1,31 @@
+const firstName= "Shevourn"
+let age = 23 
+console.log(firstName, age)
+let height;
+let heightInFeet = 5;
+let inches = 2;
+
+height = heightInFeet * 12 
+height += inches
+
+//height = height + inches 
+
+connsole.log(height)  
+
+let isCool = true
+// height = height  + inches
+
+let tempLit ='Hi my name is ${FirstName}! I am ${age} years old and it is ${iscool}.'
+let string = 'Hi my name is ${FirstName}! I am ${age} years old and it is ${iscool}.'
+
+// console.log(string)
+
+let score = 102;
+let score2 =115;
+
+if (score > score2){
+    console.log('first score is bigger or equalto second')
+}else{
+    console.log('second is bigger')
+}
+
